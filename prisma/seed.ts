778f4f825378bc816807526244984a8773e42ee2
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-20 22:16:50 +0100

    create data for animal from json using forEach

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,10 @@
 import studentData from "./data/students.json";
 import { PrismaClient } from "@prisma/client";
- 
+import animalsData from "./data/animals.json";
+
 const prisma = new PrismaClient();
- 
-const seed = async () => {
+
+const seedStudents = async () => {
   // Seed every student
   for (let i = 0; i < studentData.length; i++) {
     const thisStudent = studentData[i];
@@ -13,5 +14,20 @@ const seed = async () => {
   }
 };
 
-seed();
+seedStudents();
 // console.log("seed")
+
+const seedAnimals = () => {
+  animalsData.forEach(async (animal) => {
+    await prisma.animal.create({
+      data: {
+        name: animal.name,
+        kind: animal.kind,
+        age: animal.age,
+        hasBeenFed: animal.hasBeenFed,
+        imgUrl: animal.imgUrl,
+      },
+    });
+  });
+};
+seedAnimals();
